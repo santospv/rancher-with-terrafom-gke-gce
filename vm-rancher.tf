15451cb3898ac77f2d7c32e0ef24bbeb21b55798
Paulo Vitor dos Santos <paulovitor8@gmail.com>
2023-02-08 11:23:37 -0300

    Subindo Projeto inicial

---
+++ vm-rancher.tf
@@ -0,0 +1,24 @@
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+resource "google_compute_instance" "rancher_host" {
+  name = sensitive("${var.project_id}-rancher")
+  project = var.project_id
+  machine_type = "e2-medium"
+  zone = var.zone
+  tags = ["gce", sensitive("${var.project_id}-rancher")]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size = "15"
+    }
+  }
+
+  network_interface {
+    network    = "default"
+    # subnetwork = google_compute_subnetwork.subnet.name
+    access_config {
+    }
+  }
+}
