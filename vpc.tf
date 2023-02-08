16711a97ea08be01db5c79e954770656233a89aa
Paulo Vitor dos Santos <paulovitor8@gmail.com>
2023-02-08 20:53:30 -0300

    Ajustes variables e subnets

--- vpc.tf
+++ vpc.tf
@@ -5,40 +5,13 @@ resource "google_compute_network" "terraform-network" {
 }
 resource "google_compute_subnetwork" "public_subnet" {
   name          = sensitive("${var.project_id}-public-subnet")
-  ip_cidr_range = var.uc1_public_subnet
+  ip_cidr_range = var.public_subnet
   network       = google_compute_network.terraform-network.name
   region        = var.region
 }
 resource "google_compute_subnetwork" "private_subnet" {
   name          = sensitive("${var.project_id}-private-subnet")
-  ip_cidr_range = var.uc1_private_subnet
+  ip_cidr_range = var.private_subnet
   network       = google_compute_network.terraform-network.name
   region        = var.region
 }
-
-# resource "google_compute_router" "router" {
-#   name    = "my-router"
-#   region  = google_compute_subnetwork.subnet.region
-#   network = google_compute_network.terraform-network.name
-# }
-# resource "google_compute_address" "static" {
-#   name = "ipv4-address2"
-#   region = google_compute_subnetwork.subnet.region
-# }
-
-# resource "google_compute_router_nat" "nat_rules" {
-#   name   = "my-router-nat"
-#   router = google_compute_router.router.name
-#   region = google_compute_router.router.region
-
-#   nat_ip_allocate_option = "MANUAL_ONLY"
-#   nat_ips                = [google_compute_address.static.self_link]
-
-#   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-#   subnetwork {
-#     name                    = google_compute_subnetwork.subnet.id
-#     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-#   }
-
-#   enable_endpoint_independent_mapping = false
-# }
